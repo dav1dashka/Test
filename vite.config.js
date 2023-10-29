c920562f7d8990fbbdd6796485fa08a587fade0a
dav1dashka <135046682+dav1dashka@users.noreply.github.com>
2023-10-29 19:51:56 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-  ],
-})
+  plugins: [react()],
+  base: "https://github.com/dav1dashka/Test/",
+});
